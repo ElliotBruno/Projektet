8db31264a33a4ed8d914b5e1c339139a56ee289f
Elliot <elliot.bruno@skola.taby.se>
2024-03-14 12:55:04 +0100

    first commit

---
+++ webb.js
@@ -0,0 +1,23 @@
+// const express = require('express')
+// const app = express()
+// const port = 3001
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/webb.html');
+// })
+
+
+
+// app.listen(port, () => {
+//   console.log(`App listening at http://localhost:${port}`)
+// })
+
+
+// app.get('/greet', (req, res) => {
+//   const name = req.query.name
+//   let html=`<html><head></head><body><h1>Hej ${name} </h1></body></html>`;
+//   res.send(html)
+// })
+
+
+// Ska ta bort byta ut mot php
---
+++ webb.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Guestbook</title>
+</head>
+
+<body>
+
+    <?php
+
+    $servername = "localhost"; // kopplar till din lokala databas som körs i xampp
+    $username = "root";
+    $password = "";
+    $db = "inlämning4"; //använd namnet på din databas
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $db);
+    $sql = "SELECT * FROM guestbook ORDER BY time desc";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo ("<hr>
+            Posts: <br>
+            <br>
+            " . $row["time"] . "<br>
+            From: " . $row["name"] . "<br>
+            Email: " . $row["email"] . "<br>
+            <a href='" . $row["homepage"] . "'>Homepage</a>
+            <br>
+            Comment: " . $row["comment"] . "<br>
+
+            <div style='width: 100%'>
+    <iframe width='50%' height='300' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.com/maps?width=100%%26amp;height=600&amp;hl=en&amp;q=" . $row["address"] . "+(Your%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'>
+        <a href='https://www.gps.ie/sport-gps/'>hiking gps</a>
+    </iframe>
+</div>
+
+
+            "
+
+            );
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+
+
+
+
+
+
+    ?>
+
+</body>
+
+
+
+</html>
