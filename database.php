4a8f853ba0f5643fe023266af9e706d8e4c6a8ab
Elliot <elliot.bruno@skola.taby.se>
2024-04-17 17:40:35 +0200

    databas

---
+++ database.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $servername = "localhost"; // kopplar till din lokala databas som körs i xampp
+    $username = "root";
+    $password = "";
+    $db = "php"; //använd namnet på din databas
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $db);
+
+
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+
+
+    $sql = "SELECT * FROM upload";
+    $result = $conn->query($sql);
+
+
+    $login_success = false;
+    $full_name = "";
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if (
+                $row["username"] == $_POST["username"] &&
+                $row["password"] == $_POST["password"]
+            ) {
+                $login_success = true;
+                $full_name = $row["name"];
+            }
+        }
+    } else {
+        echo "0 results";
+    }
+    if ($login_success) {
+        echo "Connected successfully" . "<br>";
+        echo "Hej " . $full_name . "<br>";
+        echo "<a href='Upload6.php'>Ladda upp fil</a>";
+        $_SESSION["username"] = $_POST["username"];
+    } else {
+        echo "Inloggning misslyckades";
+    }
+    $conn->close();
+
+    if ($login_success) {
+        $_SESSION["username"] = $_POST["username"];
+    }
+    ?>
+    <form action="inlämning6.html">
+        <input type="submit" value="Log out" />
+    </form>
+</body>
+
+</html>
